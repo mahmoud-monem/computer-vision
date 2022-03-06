2addde85f1978e9ad35fb5d25f1fe6a0e73ecc8d
Mahmoud Monem <mahmoud.monem.elgohary@gmail.com>
2022-03-06 18:27:01 +0200

    image model

--- src/common/image-model.py
+++ src/common/image-model.py
@@ -5,15 +5,29 @@ import numpy as np
 class ImageModel:
     image_gray: np.ndarray
     image_rgb: np.ndarray
-    image_bgr: np.ndarray
-    image_path: str
 
-    def __init__(self):
+    def __init__(self, path: str):
+        self.image_rgb = cv.imread('assets/images/spartacus.jpg')
+        self.image_gray = self.rgb2gray(self.image_rgb)
         pass
 
     @staticmethod
-    def rgb2gray(img: np.ndarray):
+    def rgba2rgb(rgba):
+        return rgba[:, :, :-1]
+
+    @staticmethod
+    def rgb2gray(img: np.ndarray) -> np.ndarray:
+        ret: np.ndarray
         if (img.ndim == 2):
-            assert 'The image is already gray'
+            ret = img
+        elif img.shape[-1] == 4:
+            print("rgba image to gray")
+            img = ImageModel.rgba2rgb(img)
+            ret = np.dot(img[..., :3], [0.299, 0.587, 0.114])
         else:
-            return np.dot(img[..., :3], [0.299, 0.587, 0.114])
+            ret = np.dot(img[..., :3], [0.299, 0.587, 0.114])
+        return ret
+
+
+if __name__ == '__main__':
+    img = ImageModel()
